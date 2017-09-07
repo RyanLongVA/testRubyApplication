65731c17f01fa9927ca4ffb6b9d3eb76dbd425f3
Ryan Long <saferkeymercy@gmail.com>
2017-09-07 14:05:14 -0400

    fixed message?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-    def hello
-        render html: "test, success"
-    end
+  def hello
+    render html: "test, success"
+  end
 end
